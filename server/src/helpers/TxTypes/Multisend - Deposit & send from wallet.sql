017a7657faf2da8d6f43e42b38edd18707a83fd7
Oleh Ivaniuk <fedencer7@gmail.com>
2022-08-10 18:13:49 +0300

    Fixed for Multi-send transactions

--- server/src/helpers/TxTypes/Multisend - Deposit & send from wallet.sql
+++ server/src/helpers/TxTypes/Multisend - Deposit & send from wallet.sql
@@ -10,12 +10,12 @@ FROM receipts r
     INNER JOIN execution_outcomes e ON e.receipt_id = r.receipt_id
     INNER JOIN blocks b ON b.block_hash = r.included_in_block_hash
     INNER JOIN action_receipt_actions ra ON ra.receipt_id = r.receipt_id
-WHERE r.receiver_account_id = 'multisender.app.near'
+WHERE r.receiver_account_id IN ('multisender.app.near', 'bulksender.near')
   AND r.predecessor_account_id = $1
   AND e.status = 'SUCCESS_VALUE'
   AND ra.action_kind = 'FUNCTION_CALL'
   AND ra.args ->> 'args_json'::text IS NOT NULL
-  AND ra.args ->> 'method_name'::text = 'multisend_attached_tokens'
+  AND ra.args ->> 'method_name'::text IN ('multisend_attached_tokens', 'distribute_near')
   AND b.block_timestamp > $2
   AND EXISTS(
     SELECT 1
