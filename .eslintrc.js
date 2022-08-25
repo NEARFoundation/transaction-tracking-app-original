251aecc9bd4cfb675ddfc0c6cfbfe1790723a0cb
Ryan Walsh <rcwalsh@gmail.com>
2022-08-25 14:50:44 -0400

    quieting incorrect no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: ['near'],
+  extends: ['near', 'eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended'], // https://stackoverflow.com/a/58513127/
   globals: {
     console: true,
     process: true,
