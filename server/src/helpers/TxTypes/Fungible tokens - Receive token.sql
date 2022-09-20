d48067606e09622671be2d8f8fe6d8b6950e77b9
Ryan Walsh <rcwalsh@gmail.com>
2022-09-20 16:54:25 -0400

    improved query to not need hard-coded allow-list

--- server/src/helpers/TxTypes/Fungible tokens - Receive token.sql
+++ server/src/helpers/TxTypes/Fungible tokens - Receive token.sql
@@ -5,7 +5,7 @@ SELECT b.block_timestamp,
        convert_from(decode(ara.args ->> 'args_base64', 'base64'), 'UTF8') args_base64,
        r.originated_from_transaction_hash transaction_hash,
        ara.args ->'args_json'->>'amount' amount_transferred,
-       'token' currency_transferred,
+       'token' currency_transferred, /* TODO: Show the actual token symbol? */
        r.receiver_account_id receiver_owner_account
 FROM receipts r
     INNER JOIN execution_outcomes eo ON eo.receipt_id = r.receipt_id
@@ -15,9 +15,8 @@ WHERE
   eo.status IN ('SUCCESS_RECEIPT_ID', 'SUCCESS_VALUE')
   AND ara.action_kind = 'FUNCTION_CALL'
   AND COALESCE(ara.args::json->>'method_name', '') = 'ft_transfer'
-  AND COALESCE((ara.args::json->'args_json')::json->>'receiver_id', '') = $1
+  AND (ara.args -> 'args_json'::text) ->> 'receiver_id'::text = $1
   AND b.block_timestamp > $2
-  AND r.predecessor_account_id IN ('nf-payments.near') /* TODO: List all the possible accounts that might send FT... or... figure out a way to speed up the query without this condition. */
 
 ORDER BY block_timestamp
 LIMIT $3
