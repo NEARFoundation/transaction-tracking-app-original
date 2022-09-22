4091194f18afcc1ba1ff622eb0449b2addab598c
Ryan Walsh <rcwalsh@gmail.com>
2022-09-22 15:30:31 -0400

    now all test cases pass for roundAsLocaleStringWithUnitLabel

--- shared/helpers/nearUnits.test.ts
+++ shared/helpers/nearUnits.test.ts
@@ -1,17 +1,12 @@
 // https://jestjs.io/docs/getting-started#using-typescript
 
 import { NEAR } from 'near-units'; // https://github.com/near/units-js
-import { roundAsLocaleStringWithUnitLabel, round, getDenominator } from './nearUnits';
+import { roundAsLocaleStringWithUnitLabel, round } from './nearUnits';
 
 describe('nearUnits', () => {
-  test('getDenominator', () => {
-    expect(getDenominator('yN').toString()).toBe('1');
-    expect(getDenominator('N').toString()).toBe(NEAR.parse('1 N').toString());
-    expect(getDenominator('kN').toString()).toBe(NEAR.parse('1 kN').toString());
-  });
-
   test('round', () => {
     const cases = {
+      '9999_513_263_875_304_671_192_000_009': '9999.513264',
       '4_513_263_875_304_671_192_000_009': '4.513264',
       '19_986_790_304_670_292_625_563': '0.019987',
       '9_993_575_646_028_206_228_514': '0.009994',
@@ -24,28 +19,30 @@ describe('nearUnits', () => {
 
   test('roundAsLocaleStringWithUnitLabel', () => {
     const cases = {
+      '9999_513_263_875_304_671_192_000_009': '9,999.513264 N',
       '4_513_263_875_304_671_192_000_009': '4.513264 N',
       '19_986_790_304_670_292_625_563': '0.019987 N',
       '9_993_575_646_028_206_228_514': '0.009994 N',
-      // TODO '4_500_000_000': '0.000000 N',// TODO: Honor decimals precision by padding with extra zeroes if necessary
-      '9999_513_263_875_304_671_192_000_009': '9,999.513264 N',
+      '4_500_000_000': '0.000000 N',
     };
     Object.keys(cases).forEach((key) => {
       expect(roundAsLocaleStringWithUnitLabel(key)).toBe(cases[key]);
     });
   });
 
-  test('roundAsLocaleStringWithUnitLabel custom power and decimals', () => {
+  test('roundAsLocaleStringWithUnitLabel custom unitLabel and decimals', () => {
     const cases = {
-      '19986790304670292625563': '19.99 mN',
-      '9993575646028206228514': '9.99 mN',
-      '4500000000': '0 mN',
+      '19_986_790_304_670_292_625_563': '19.99 mN',
+      '9_993_575_646_028_206_228_514': '9.99 mN',
+      '4_500_000_000': '0.00 mN',
     };
     Object.keys(cases).forEach((key) => {
       const decimals = 2;
       expect(roundAsLocaleStringWithUnitLabel(key, 'mN', decimals)).toBe(cases[key]);
     });
 
-    // expect(roundAsLocaleStringWithUnitLabel('1 N', 'mN', 4)).toBe('0.0001 mN'); // TODO
+    expect(roundAsLocaleStringWithUnitLabel('500_000_000_000_000', 'nN', 4)).toBe('0.5000 nN');
+    expect(roundAsLocaleStringWithUnitLabel('490', 'zN', 1)).toBe('0.5 zN');
+    expect(roundAsLocaleStringWithUnitLabel('1', 'yN', 0)).toBe('1 yN');
   });
 });
--- shared/helpers/nearUnits.ts
+++ shared/helpers/nearUnits.ts
@@ -3,12 +3,6 @@
 import { NEAR } from 'near-units'; // https://github.com/near/units-js
 import exactMath from 'exact-math'; // https://www.npmjs.com/package/exact-math
 
-export function getDenominator(unitLabel: string): string {
-  const denominator = NEAR.parse(`1 ${unitLabel}`);
-  // console.log('denominator', denominator.toString());
-  return denominator.toString();
-}
-
 /**
  *
  * @param yoctonear {string}
@@ -18,7 +12,8 @@ export function getDenominator(unitLabel: string): string {
  */
 export function round(yoctonear: string, unitLabel = 'N', decimals = 6): string {
   const original = yoctonear.replaceAll('_', '');
-  const denominator = getDenominator(unitLabel);
+  const denominator = NEAR.parse(`1 ${unitLabel}`).toString();
+  // console.log(original, denominator);
   const value = exactMath.div(original, denominator);
   // console.log(`round(${original}, power = ${unitLabel}, decimals = ${decimals}) = ${value.toString()}`, denominator.toString());
 
@@ -35,9 +30,12 @@ export function round(yoctonear: string, unitLabel = 'N', decimals = 6): string 
 export function roundAsLocaleStringWithUnitLabel(yoctonear: string, unitLabel = 'N', decimals = 6): string {
   const value = round(yoctonear, unitLabel, decimals);
   // console.log({ value, unitLabel });
-  return `${Number(value).toLocaleString(undefined, {
+  const localeString = Number(value).toLocaleString(undefined, {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#options https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
     //useGrouping: 'auto',
     signDisplay: 'auto',
+    minimumFractionDigits: decimals, // Honor decimals precision by padding with extra zeroes if necessary
     maximumFractionDigits: decimals,
-  })} ${unitLabel}`; // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#options https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+  });
+  return `${localeString} ${unitLabel}`;
 }
