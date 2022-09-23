d7e8388a3e5addb16392438387c6f8a7b4f7d4bb
Ryan Walsh <rcwalsh@gmail.com>
2022-09-23 12:39:19 -0400

    progress on precision helper but a few test cases still fail

--- shared/helpers/precision.test.ts
+++ shared/helpers/precision.test.ts
@@ -1,14 +1,26 @@
 // https://jestjs.io/docs/getting-started#using-typescript
 
 import exactMath from 'exact-math'; // https://www.npmjs.com/package/exact-math
-import { round } from './precision';
+import { round, getLocaleStringToDecimals } from './precision';
 
 describe('precision helper', () => {
+  const locale = 'en-US';
+  const deDE = 'de-DE';
+
   test('exact-math', () => {
     expect(exactMath.div('9999513263875304671192000009', 1, { returnString: true })).toBe('9999513263875304671192000009');
   });
 
+  test('getLocaleStringToDecimals', () => {
+    expect(getLocaleStringToDecimals('4500000000', 1, locale)).toBe('4,500,000,000.0');
+    expect(getLocaleStringToDecimals('9000.0000000000004500000000', 15, locale)).toBe('9,000.000000000000450');
+    expect(getLocaleStringToDecimals('9000.630', 3, deDE)).toBe('9.000,630');
+    expect(getLocaleStringToDecimals(exactMath.formula('4.5e-12', { returnString: true }), 2, locale)).toBe('0.00'); // TODO
+    expect(getLocaleStringToDecimals('4_513_263_875_304_671_192_000_009.199867903046702926255639', 20, locale)).toBe('4,513,263,875,304,671,192,000,009.19986790304670292625'); // TODO
+  });
+
   test('round (using defaults)', () => {
+    // The cases in this test would sometimes fail if not specifying `locale` too, actually, so `undefined` is used below to cause the defaults to be used for those arguments.
     const cases = {
       '9_999_513_263_875_304_671_192_000_009': '9,999,513,263,875,304,671,192,000,009',
       '4_513_263_875_304_671_192_000_009': '4,513,263,875,304,671,192,000,009',
@@ -17,25 +29,27 @@ describe('precision helper', () => {
       '4_500_000_000': '4,500,000,000',
     };
     Object.keys(cases).forEach((key) => {
-      expect(round(key)).toBe(cases[key]);
+      expect(round(key, undefined, undefined, locale)).toBe(cases[key]);
     });
-    expect(round('1')).toBe('1');
+    expect(round('1', undefined, undefined, locale)).toBe('1');
   });
 
   test('round custom decimals and divisorPower', () => {
     const cases = {
       '19_986_790_304_670_292_625_563': '19.99',
       '9_993_575_646_028_206_228_514': '9.99',
-      '4_500_000_000': '0.00',
+      '4_500_000_000': '0.00', // TODO
     };
     Object.keys(cases).forEach((key) => {
       const decimals = 2;
       const divisorPower = 21;
-      expect(round(key, decimals, divisorPower)).toBe(cases[key]);
+      expect(round(key, decimals, divisorPower, locale)).toBe(cases[key]);
     });
 
-    expect(round('500_000_000_000_000', 4, 15)).toBe('0.5000');
-    expect(round('490', 1, 3)).toBe('0.5');
-    expect(round('4_513_263_875_304_671_192_000_009', 6, 21)).toBe('4,513.263875');
+    expect(round('500_000_000_000_000', 4, 15, locale)).toBe('0.5000');
+    expect(round('490', 1, 3, locale)).toBe('0.5');
+    expect(round('4_513_263_875_304_671_192_000_009', 6, 21, locale)).toBe('4,513.263875');
+    expect(round('4_513_263_875_304_671_192_000_009', 6, 21, deDE)).toBe('4.513,263875');
+    expect(round('4_513_263_875_304_671_192_000_009.199867903046702926255639', 20, undefined, locale)).toBe('4,513,263,875,304,671,192,000,009.19986790304670292625'); // TODO
   });
 });
--- shared/helpers/precision.ts
+++ shared/helpers/precision.ts
@@ -1,32 +1,50 @@
 import exactMath from 'exact-math'; // https://www.npmjs.com/package/exact-math
 
-function getLocaleStringToDecimals(value: number, decimals: number): string {
-  return value.toLocaleString(undefined, {
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#options https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
-    //useGrouping: 'auto',
-    signDisplay: 'auto',
-    minimumFractionDigits: decimals, // Honor decimals precision by padding with extra zeroes if necessary
-    maximumFractionDigits: decimals,
-  });
+/**
+ *
+ * @param amount {string}
+ * @param decimals {number} e.g. 6 would return 6 decimal places like 0.000000
+ * @param locale {string} e.g. 'en-US' or 'de-DE'
+ * @returns {string} e.g. 1,000.000000
+ */
+export function getLocaleStringToDecimals(amount: string, decimals: any, locale?: string): string {
+  // Thanks to https://stackoverflow.com/a/68906367/ because https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt/toLocaleString would not work for huge numbers or numbers with many decimal places.
+
+  if (decimals > 20) {
+    throw new Error(
+      'decimals must be <= 20. See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#minimumfractiondigits',
+    );
+  }
+  const [mainString, decimalString] = amount.split('.'); // ['321321321321321321', '357' | '998']
+  const decimalFormat = new Intl.NumberFormat(locale, { minimumFractionDigits: decimals, maximumFractionDigits: decimals });
+  const decimalFullString = `0.${decimalString}`; // '0.357' | '0.998'
+  const decimalFullNumber = Number.parseFloat(decimalFullString); // 0.357 | 0.998
+  const decimalFullFinal = decimalFormat.format(decimalFullNumber); // '0,36' | '1,00'
+  const decimalFinal = decimalFullFinal.slice(1); // ',36' | ',00'
+  const mainFormat = new Intl.NumberFormat(locale, { minimumFractionDigits: 0 });
+  let mainBigInt = BigInt(mainString); // 321321321321321321n
+  if (decimalFullFinal[0] === '1') mainBigInt += BigInt(1); // 321321321321321321n | 321321321321321322n
+  const mainFinal = mainFormat.format(mainBigInt); // '321.321.321.321.321.321' | '321.321.321.321.321.322'
+  const amountFinal = `${mainFinal}${decimalFinal}`; // '321.321.321.321.321.321,36' | '321.321.321.321.321.322,00'
+  return amountFinal;
 }
 
 /**
  *
- * @param original {string}
+ * @param amount {string}
  * @param decimals {number} e.g. 6 would return 6 decimal places like 0.000000
  * @param divisorPower {number} e.g. 0 for yocto, 24 for [base], 27 for kilo, etc https://docs.near.org/tools/near-api-js/utils https://github.com/near/units-js/blob/d0e76d5729b0f3b58b98263a1f92fb057eb84d96/__tests__/near.spec.ts#L13
- * @returns {string} e.g. 1000.000000
+ * @param locale {string} e.g. 'en-US' or 'de-DE'
+ * @returns {string} e.g. 1,000.000000
  */
-export function round(original: string, decimals = 0, divisorPower = 0): string {
+export function round(amount: string, decimals = 0, divisorPower = 0, locale?: string): string {
   if (divisorPower < 0) {
     throw new Error('divisorPower must be >= 0');
   }
-  const originalCleaned = original.replaceAll('_', '');
+  const amountCleaned = amount.replaceAll('_', '');
   const divisor = Math.pow(10, divisorPower);
-  // console.log(original, divisor);
-  const value: string = exactMath.div(originalCleaned, divisor, { returnString: true });
-  // console.log(`round(${original}, decimals = ${decimals}, divisorPower = ${divisorPower}) = ${value}`);
-
-  const localeString = getLocaleStringToDecimals(Number(value), decimals); // TODO: Fix this. Currently large numbers like 9,999,513,263,875,304,671,192,000,009 get rounded to 9,999,513,263,875,304,000,000,000,000 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger
+  const value: string = exactMath.div(amountCleaned, divisor, { returnString: true });
+  // console.log(`round(${amount}, decimals = ${decimals}, divisorPower = ${divisorPower}) = ${value}`, divisor);
+  const localeString = getLocaleStringToDecimals(value, decimals, locale);
   return localeString;
 }
