074882398288b37b9258ccf048e4e80f5a3c6d12
Ryan Walsh <rcwalsh@gmail.com>
2022-10-21 16:17:04 -0400

    use loop to reduce duplication

--- shared/helpers/logging.ts
+++ shared/helpers/logging.ts
@@ -4,18 +4,21 @@ import chalk from 'chalk'; // https://www.npmjs.com/package/chalk
 
 import { getFormattedUtcDatetimeNow } from './datetime.js';
 
-const error = chalk.bold.red;
-const warn = chalk.hex('#FFA500'); // Orange color
 const success = chalk.green;
-const info = chalk.gray;
-const debug = chalk.blue;
+
+const styles = {
+  error: chalk.bold.red,
+  warn: chalk.hex('#FFA500'), // Orange color
+  info: chalk.gray,
+  debug: chalk.blue,
+};
 
 type Arguments = any;
 
 function getArgumentsPreserved(providedArguments: Arguments): string {
   // https://nodejs.org/en/knowledge/getting-started/how-to-use-util-inspect/
   // https://github.com/chalk/chalk/issues/118#issuecomment-1221385194
-  return util.inspect(providedArguments, { colors: true, depth: null });
+  return util.inspect(providedArguments, { colors: false, depth: null });
 }
 
 const originalLog = console.log; // https://developer.mozilla.org/en-US/docs/Web/API/console // https://stackoverflow.com/a/16259739/
@@ -23,25 +26,13 @@ console.log = function (...providedArguments: Arguments) {
   Reflect.apply(originalLog, this, [getFormattedUtcDatetimeNow(), ...providedArguments]);
 };
 
-const originalInfo = console.info;
-console.info = function (...providedArguments: Arguments) {
-  Reflect.apply(originalInfo, this, [getFormattedUtcDatetimeNow(), info(getArgumentsPreserved(providedArguments))]);
-};
-
-const originalDebug = console.debug;
-console.debug = function (...providedArguments: Arguments) {
-  Reflect.apply(originalDebug, this, [getFormattedUtcDatetimeNow(), debug(getArgumentsPreserved(providedArguments))]);
-};
-
-const originalWarn = console.warn;
-console.warn = function (...providedArguments: Arguments) {
-  Reflect.apply(originalWarn, this, [getFormattedUtcDatetimeNow(), warn(getArgumentsPreserved(providedArguments))]);
-};
-
-const originalError = console.error;
-console.error = function (...providedArguments: Arguments) {
-  Reflect.apply(originalError, this, [getFormattedUtcDatetimeNow(), error(getArgumentsPreserved(providedArguments))]);
-};
+for (const style of Object.keys(styles)) {
+  const originalStyle = console[style];
+  const callback = styles[style];
+  console[style] = function (...providedArguments: Arguments) {
+    Reflect.apply(originalStyle, this, [getFormattedUtcDatetimeNow(), callback(getArgumentsPreserved(providedArguments))]);
+  };
+}
 
 /**
  * NOTE: Currently, importing this function is the only way that console.log, console.warn, and other functions get overwritten above.
