dfa8a176d1c51913abf7c29e8ca48291d1cceffa
Ryan Walsh <rcwalsh@gmail.com>
2022-10-21 16:01:18 -0400

    improve chalk logging via getArgumentsPreserved

--- shared/helpers/logging.ts
+++ shared/helpers/logging.ts
@@ -1,3 +1,5 @@
+import util from 'util';
+
 import chalk from 'chalk'; // https://www.npmjs.com/package/chalk
 
 import { getFormattedUtcDatetimeNow } from './datetime.js';
@@ -8,38 +10,44 @@ const success = chalk.green;
 const info = chalk.gray;
 const debug = chalk.blue;
 
-type Args = any;
+type Arguments = any;
+
+function getArgumentsPreserved(providedArguments: Arguments): string {
+  // https://nodejs.org/en/knowledge/getting-started/how-to-use-util-inspect/
+  // https://github.com/chalk/chalk/issues/118#issuecomment-1221385194
+  return util.inspect(providedArguments, { colors: true, depth: null });
+}
 
 const originalLog = console.log; // https://developer.mozilla.org/en-US/docs/Web/API/console // https://stackoverflow.com/a/16259739/
-console.log = function (...args: Args) {
-  Reflect.apply(originalLog, this, [getFormattedUtcDatetimeNow(), ...args]);
+console.log = function (...providedArguments: Arguments) {
+  Reflect.apply(originalLog, this, [getFormattedUtcDatetimeNow(), ...providedArguments]);
 };
 
 const originalInfo = console.info;
-console.info = function (...args: Args) {
-  Reflect.apply(originalInfo, this, [getFormattedUtcDatetimeNow(), info(...args)]);
+console.info = function (...providedArguments: Arguments) {
+  Reflect.apply(originalInfo, this, [getFormattedUtcDatetimeNow(), info(getArgumentsPreserved(providedArguments))]);
 };
 
 const originalDebug = console.debug;
-console.debug = function (...args: Args) {
-  Reflect.apply(originalDebug, this, [getFormattedUtcDatetimeNow(), debug(...args)]);
+console.debug = function (...providedArguments: Arguments) {
+  Reflect.apply(originalDebug, this, [getFormattedUtcDatetimeNow(), debug(getArgumentsPreserved(providedArguments))]);
 };
 
 const originalWarn = console.warn;
-console.warn = function (...args: Args) {
-  Reflect.apply(originalWarn, this, [getFormattedUtcDatetimeNow(), warn(...args)]);
+console.warn = function (...providedArguments: Arguments) {
+  Reflect.apply(originalWarn, this, [getFormattedUtcDatetimeNow(), warn(getArgumentsPreserved(providedArguments))]);
 };
 
 const originalError = console.error;
-console.error = function (...args: Args) {
-  Reflect.apply(originalError, this, [getFormattedUtcDatetimeNow(), error(...args)]);
+console.error = function (...providedArguments: Arguments) {
+  Reflect.apply(originalError, this, [getFormattedUtcDatetimeNow(), error(getArgumentsPreserved(providedArguments))]);
 };
 
 /**
  * NOTE: Currently, importing this function is the only way that console.log, console.warn, and other functions get overwritten above.
  *
- * @param args
+ * @param providedArguments
  */
-export function logSuccess(...args: Args) {
-  console.log(success(...args));
+export function logSuccess(...providedArguments: Arguments) {
+  console.log(success(getArgumentsPreserved(providedArguments)));
 }
