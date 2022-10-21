099d53d3fb334d62963e450094453b005f2663b3
Ryan Walsh <rcwalsh@gmail.com>
2022-10-21 14:08:51 -0400

    revert syncedCron to be more like https://github.com/NEARFoundation/tx-tracking-app/blob/dee2509eed04b65095eb5c4856eefda2cb659daf/server/src/helpers/syncedCron.js

--- backend/src/helpers/syncedCron.ts
+++ backend/src/helpers/syncedCron.ts
@@ -5,35 +5,12 @@ import { logSuccess } from '../../../shared/helpers/logging.js';
 import { CRON_SCHEDULE } from './config.js';
 import { runAllNonRunningTasks } from './updateTransactions.js';
 
-let isAlreadyRunningBoolean = false;
-
-function isAlreadyRunning(): boolean {
-  return isAlreadyRunningBoolean;
-}
-
 export const SyncedCron = cron.schedule(
   CRON_SCHEDULE,
   async () => {
-    // console.log('========================================');
-    // console.log('SyncedCron is checking the value of isAlreadyRunning...');
-
-    if (isAlreadyRunning() === false) {
-      // console.log('Calling runAllNonRunningTasks() now...');
-      try {
-        isAlreadyRunningBoolean = true;
-        console.info('awaiting runAllNonRunningTasks() now (and isAlreadyRunningBoolean=1)');
-
-        await runAllNonRunningTasks();
-
-        logSuccess('Finished awaiting runAllNonRunningTasks.');
-        isAlreadyRunningBoolean = false;
-        logSuccess(`Finished cron, so isAlreadyRunningBoolean = ${isAlreadyRunningBoolean}`);
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      console.log('SyncedCron: runAllNonRunningTasks is already running.');
-    }
+    console.info('Awaiting runAllNonRunningTasks().');
+    await runAllNonRunningTasks();
+    logSuccess('Finished awaiting runAllNonRunningTasks. (Finished cron.)');
   },
   {
     scheduled: false,
