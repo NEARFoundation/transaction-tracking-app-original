a5d7a0aed27bf97d4f0e67d021f8d914ff246ef5
Ryan Walsh <rcwalsh@gmail.com>
2022-10-31 11:11:18 -0400

    Call runAllNonRunningTasks() once before cron job even starts.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,12 @@ import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { logger } from '../../shared/helpers/logging.js';
+
 import { addDefaultTypesTx } from './helpers/addDefaultTypesTx.js';
 import { mongoConnectionString, port } from './helpers/config.js';
 import { SyncedCron } from './helpers/syncedCron.js';
+import { runAllNonRunningTasks } from './helpers/updateTransactions.js';
 import { routes } from './routes/collector.routes.js';
 
 await mongoose.connect(mongoConnectionString);
@@ -16,4 +19,8 @@ routes(app);
 app.listen(port);
 
 await addDefaultTypesTx();
+
+logger.debug('Awaiting runAllNonRunningTasks().');
+await runAllNonRunningTasks(); // Call once before cron job even starts.
+
 SyncedCron.start();
