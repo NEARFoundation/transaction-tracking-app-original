d6efa212f0dcbcbd3b1f153abb2a7be08a09ecec
Ryan Walsh <rcwalsh@gmail.com>
2022-10-31 11:08:15 -0400

    dropActionsAndTasksAndTypes

--- backend/dropActionsAndTasksAndTypes.ts
+++ backend/dropActionsAndTasksAndTypes.ts
@@ -1,10 +1,11 @@
-// Run via `yarn dropActionsAndTasks` or `yarn ts-node --esm ./backend/dropActionsAndTasks.ts`.
+// Run via `yarn dropActionsAndTasksAndTypes` or `yarn ts-node --esm ./backend/dropActionsAndTasksAndTypes.ts`.
 
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 import { TxActions } from './src/models/TxActions.js';
 import { TxTasks } from './src/models/TxTasks.js';
+import { TxTypes } from './src/models/TxTypes.js';
 
 const result = dotenv.config({ path: './backend/.env.development.local' });
 if (result.error) {
@@ -21,6 +22,8 @@ async function main() {
   console.log('Deleted all TxActions.');
   await TxTasks.deleteMany({});
   console.log('Deleted all TxTasks.');
+  await TxTypes.deleteMany({});
+  console.log('Deleted all TxTypes.');
   console.log('Finished.');
   process.exit(0); // https://stackoverflow.com/a/52461246/
 }
