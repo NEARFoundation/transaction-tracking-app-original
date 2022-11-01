edbfcf74ecbac6deaa0e9ea01f4521b2562917bf
Ryan Walsh <rcwalsh@gmail.com>
2022-11-01 11:43:49 -0400

    reorder functions to honor eslint no-use-before-define

--- backend/src/helpers/updateTransactions.ts
+++ backend/src/helpers/updateTransactions.ts
@@ -1,3 +1,4 @@
+/* eslint no-use-before-define: "error"*/
 import { performance } from 'perf_hooks';
 
 import { type Request, type Response } from 'express';
@@ -13,6 +14,118 @@ import { TxTypes } from '../models/TxTypes.js';
 
 import { CONNECTION_STRING, CONNECTION_TIMEOUT, DEFAULT_LENGTH, STATEMENT_TIMEOUT } from './config.js';
 import { getCurrencyByPool, getCurrencyByContract } from './getCurrency.js';
+
+async function getMostRecentBlockTimestamp(accountId: AccountId, txType: string): Promise<number> {
+  const mostRecentTxAction = await TxActions.findOne({
+    accountId,
+    txType,
+  }).sort({ block_timestamp: 'desc' });
+  const mostRecentBlockTimestamp = mostRecentTxAction ? mostRecentTxAction.block_timestamp : 0;
+  // logger.info(`getMostRecentBlockTimestamp(${accountId}, ${txType})`, mostRecentBlockTimestamp);
+  return Number(mostRecentBlockTimestamp); // backend/src/models/TxActions.js uses Decimal128 for this field, which React can't display. https://thecodebarbarian.com/a-nodejs-perspective-on-mongodb-34-decimal.html
+}
+
+async function getTransactions(pgClient: Client, accountId: AccountId, txTypeName: string, blockTimestamp: number, length: number): Promise<TxActionRow[]> {
+  try {
+    const txType: TxTypeRow | null = await TxTypes.findOne({ name: txTypeName });
+    if (txType) {
+      logger.info(`getTransactions(${accountId}, ${txTypeName}, ${getFormattedDatetimeUtcFromBlockTimestamp(blockTimestamp)}, ${length})`);
+      const startTime = performance.now();
+      const result = await pgClient.query(txType.sql, [accountId, blockTimestamp.toString(), length]);
+      const endTime = performance.now();
+      logger.info(
+        `pgClient performance of getTransactions(${accountId}, ${txTypeName}, ${getFormattedDatetimeUtcFromBlockTimestamp(blockTimestamp)}, ${length})`,
+        millisToMinutesAndSeconds(endTime - startTime),
+      );
+      const rows = result.rows as unknown as TxActionRow[];
+      logger.info(`${accountId}, ${txTypeName} rows`, JSON.stringify(rows));
+      return rows;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    logger.error(error);
+    return [];
+  }
+}
+
+async function processTransaction(accountId: AccountId, txType: string, transaction: TxActionRow): Promise<void> {
+  logger.info('processTransaction: ', accountId, transaction.transaction_hash, getFormattedDatetimeUtcFromBlockTimestamp(transaction.block_timestamp));
+  const clonedTransaction = { ...transaction };
+  if (clonedTransaction.get_currency_by_contract) {
+    logger.info('fungibleTokenContractAccountId', clonedTransaction.get_currency_by_contract);
+
+    // eslint-disable-next-line canonical/id-match
+    clonedTransaction.currency_transferred = await getCurrencyByContract(clonedTransaction.get_currency_by_contract);
+  }
+
+  if (clonedTransaction.pool_id) {
+    [clonedTransaction.currency_transferred, clonedTransaction.currency_transferred2] = await getCurrencyByPool(Number(clonedTransaction.pool_id));
+  }
+
+  try {
+    await TxActions.findOneAndUpdate({ transaction_hash: clonedTransaction.transaction_hash, txType }, getTxActionModel(accountId, txType, clonedTransaction), { upsert: true });
+    logger.info(`Saved to Mongo cache (TxActions): ${accountId} ${clonedTransaction.transaction_hash}`);
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+// eslint-disable-next-line max-lines-per-function
+export async function updateTransactions(pgClient: pg.Client, accountId: AccountId, txType: string, length: number): Promise<void> {
+  logger.info(`updateTransactions(${accountId}, ${txType})`);
+  // eslint-disable-next-line promise/valid-params
+  await TxTasks.findOneAndUpdate(
+    { accountId },
+    {
+      isRunning: true,
+    },
+  );
+  let minBlockTimestamp = await getMostRecentBlockTimestamp(accountId, txType);
+  //  logger.info({ minBlockTimestamp });
+
+  logger.debug('Awaiting getTransactions', accountId, txType);
+  let transactions = await getTransactions(pgClient, accountId, txType, minBlockTimestamp, length);
+  // logger.info({ transactions });
+  logger.info(`Starting the 'while' loop of updateTransactions ${txType}`);
+  while (transactions.length > 0) {
+    const promises: Array<Promise<void>> = [];
+    logger.info(`Pushing all processTransaction promises for ${accountId} ${txType}.`);
+
+    for (const transaction of transactions) {
+      logger.info('About to call processTransaction', transaction.transaction_hash);
+      const promise = processTransaction(accountId, txType, transaction);
+      promises.push(promise);
+    }
+
+    logger.success('Finished the `for` loop of pushing processTransaction promises (but not the `while` loop).');
+    logger.debug(`Awaiting all updateTransactions promises for ${accountId} ${txType}.`);
+    await Promise.all(promises);
+    logger.success(`Finished awaiting all promises (but still in the 'while' loop) for ${accountId} ${txType}.`);
+    // -------------------------------------------------
+    // TODO: Document what is happening in this section:
+    let nextBlockTimestamp = transactions[transactions.length - 1].block_timestamp;
+    let index = 1;
+    while (nextBlockTimestamp === minBlockTimestamp && transactions.length === length * index) {
+      index += 1;
+      const increasedLength = length * index;
+      transactions = await getTransactions(pgClient, accountId, txType, minBlockTimestamp, increasedLength);
+      nextBlockTimestamp = transactions[transactions.length - 1].block_timestamp;
+    }
+
+    if (nextBlockTimestamp === minBlockTimestamp) {
+      break;
+    }
+
+    if (index === 1) {
+      minBlockTimestamp = nextBlockTimestamp;
+      transactions = await getTransactions(pgClient, accountId, txType, minBlockTimestamp, length);
+    }
+    // -------------------------------------------------
+  }
+
+  logger.success(`Finished the 'while' loop of updateTransactions ${txType}`);
+}
 
 // eslint-disable-next-line max-lines-per-function
 async function updateThisAccount(accountId: AccountId, types: TxTypeRow[]) {
@@ -105,115 +218,3 @@ export const runAllNonRunningTasks = async (): Promise<void> => {
   await Promise.all(promisesOfAllTasks);
   logger.success('Finished awaiting all runThisTaskByAccountId promises.');
 };
-
-async function getTransactions(pgClient: Client, accountId: AccountId, txTypeName: string, blockTimestamp: number, length: number): Promise<TxActionRow[]> {
-  try {
-    const txType: TxTypeRow | null = await TxTypes.findOne({ name: txTypeName });
-    if (txType) {
-      logger.info(`getTransactions(${accountId}, ${txTypeName}, ${getFormattedDatetimeUtcFromBlockTimestamp(blockTimestamp)}, ${length})`);
-      const startTime = performance.now();
-      const result = await pgClient.query(txType.sql, [accountId, blockTimestamp.toString(), length]);
-      const endTime = performance.now();
-      logger.info(
-        `pgClient performance of getTransactions(${accountId}, ${txTypeName}, ${getFormattedDatetimeUtcFromBlockTimestamp(blockTimestamp)}, ${length})`,
-        millisToMinutesAndSeconds(endTime - startTime),
-      );
-      const rows = result.rows as unknown as TxActionRow[];
-      logger.info(`${accountId}, ${txTypeName} rows`, JSON.stringify(rows));
-      return rows;
-    } else {
-      return [];
-    }
-  } catch (error) {
-    logger.error(error);
-    return [];
-  }
-}
-
-async function getMostRecentBlockTimestamp(accountId: AccountId, txType: string): Promise<number> {
-  const mostRecentTxAction = await TxActions.findOne({
-    accountId,
-    txType,
-  }).sort({ block_timestamp: 'desc' });
-  const mostRecentBlockTimestamp = mostRecentTxAction ? mostRecentTxAction.block_timestamp : 0;
-  // logger.info(`getMostRecentBlockTimestamp(${accountId}, ${txType})`, mostRecentBlockTimestamp);
-  return Number(mostRecentBlockTimestamp); // backend/src/models/TxActions.js uses Decimal128 for this field, which React can't display. https://thecodebarbarian.com/a-nodejs-perspective-on-mongodb-34-decimal.html
-}
-
-async function processTransaction(accountId: AccountId, txType: string, transaction: TxActionRow): Promise<void> {
-  logger.info('processTransaction: ', accountId, transaction.transaction_hash, getFormattedDatetimeUtcFromBlockTimestamp(transaction.block_timestamp));
-  const clonedTransaction = { ...transaction };
-  if (clonedTransaction.get_currency_by_contract) {
-    logger.info('fungibleTokenContractAccountId', clonedTransaction.get_currency_by_contract);
-
-    // eslint-disable-next-line canonical/id-match
-    clonedTransaction.currency_transferred = await getCurrencyByContract(clonedTransaction.get_currency_by_contract);
-  }
-
-  if (clonedTransaction.pool_id) {
-    [clonedTransaction.currency_transferred, clonedTransaction.currency_transferred2] = await getCurrencyByPool(Number(clonedTransaction.pool_id));
-  }
-
-  try {
-    await TxActions.findOneAndUpdate({ transaction_hash: clonedTransaction.transaction_hash, txType }, getTxActionModel(accountId, txType, clonedTransaction), { upsert: true });
-    logger.info(`Saved to Mongo cache (TxActions): ${accountId} ${clonedTransaction.transaction_hash}`);
-  } catch (error) {
-    logger.error(error);
-  }
-}
-
-// eslint-disable-next-line max-lines-per-function
-export async function updateTransactions(pgClient: pg.Client, accountId: AccountId, txType: string, length: number): Promise<void> {
-  logger.info(`updateTransactions(${accountId}, ${txType})`);
-  // eslint-disable-next-line promise/valid-params
-  await TxTasks.findOneAndUpdate(
-    { accountId },
-    {
-      isRunning: true,
-    },
-  );
-  let minBlockTimestamp = await getMostRecentBlockTimestamp(accountId, txType);
-  //  logger.info({ minBlockTimestamp });
-
-  logger.debug('Awaiting getTransactions', accountId, txType);
-  let transactions = await getTransactions(pgClient, accountId, txType, minBlockTimestamp, length);
-  // logger.info({ transactions });
-  logger.info(`Starting the 'while' loop of updateTransactions ${txType}`);
-  while (transactions.length > 0) {
-    const promises: Array<Promise<void>> = [];
-    logger.info(`Pushing all processTransaction promises for ${accountId} ${txType}.`);
-
-    for (const transaction of transactions) {
-      logger.info('About to call processTransaction', transaction.transaction_hash);
-      const promise = processTransaction(accountId, txType, transaction);
-      promises.push(promise);
-    }
-
-    logger.success('Finished the `for` loop of pushing processTransaction promises (but not the `while` loop).');
-    logger.debug(`Awaiting all updateTransactions promises for ${accountId} ${txType}.`);
-    await Promise.all(promises);
-    logger.success(`Finished awaiting all promises (but still in the 'while' loop) for ${accountId} ${txType}.`);
-    // -------------------------------------------------
-    // TODO: Document what is happening in this section:
-    let nextBlockTimestamp = transactions[transactions.length - 1].block_timestamp;
-    let index = 1;
-    while (nextBlockTimestamp === minBlockTimestamp && transactions.length === length * index) {
-      index += 1;
-      const increasedLength = length * index;
-      transactions = await getTransactions(pgClient, accountId, txType, minBlockTimestamp, increasedLength);
-      nextBlockTimestamp = transactions[transactions.length - 1].block_timestamp;
-    }
-
-    if (nextBlockTimestamp === minBlockTimestamp) {
-      break;
-    }
-
-    if (index === 1) {
-      minBlockTimestamp = nextBlockTimestamp;
-      transactions = await getTransactions(pgClient, accountId, txType, minBlockTimestamp, length);
-    }
-    // -------------------------------------------------
-  }
-
-  logger.success(`Finished the 'while' loop of updateTransactions ${txType}`);
-}
