fb6f5f08d3914e9c28a48a7dd1d5cd949d01b704
Ryan Walsh <rcwalsh@gmail.com>
2022-11-08 17:01:59 -0500

    fix "Fungible tokens - Receive token" TTA-4

--- backend/src/helpers/TxTypes/Fungible tokens - Receive token.sql
+++ backend/src/helpers/TxTypes/Fungible tokens - Receive token.sql
@@ -6,7 +6,7 @@ SELECT b.block_timestamp,
        r.originated_from_transaction_hash transaction_hash,
        ara.args ->'args_json'->>'amount' amount_transferred,
        'token' currency_transferred, /* TODO: Show the actual token symbol (determined by the receiver at the top level instead of in the JSON args?)? */
-       r.receiver_account_id receiver_owner_account
+       ara.args->'args_json'->>'receiver_id' receiver_owner_account
 FROM receipts r
     INNER JOIN execution_outcomes eo ON eo.receipt_id = r.receipt_id
     INNER JOIN blocks b ON b.block_hash = r.included_in_block_hash
@@ -15,8 +15,8 @@ WHERE
   eo.status IN ('SUCCESS_RECEIPT_ID', 'SUCCESS_VALUE')
   AND ara.action_kind = 'FUNCTION_CALL'
   AND COALESCE(ara.args::json->>'method_name', '') = 'ft_transfer'
-  AND (ara.args -> 'args_json'::text) ->> 'receiver_id'::text = $1
+  AND ara.args -> 'args_json' ->> 'receiver_id' = $1
   AND b.block_timestamp > $2
 
-ORDER BY block_timestamp
+ORDER BY b.block_timestamp
 LIMIT $3
