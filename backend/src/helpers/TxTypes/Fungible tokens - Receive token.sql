721fd1364e8c7784afa6f5ef5397ef1d70503139
Ryan Walsh <rcwalsh@gmail.com>
2022-11-14 15:13:40 -0500

    improved speed of 'Fungible tokens - Receive token.sql'

--- backend/src/helpers/TxTypes/Fungible tokens - Receive token.sql
+++ backend/src/helpers/TxTypes/Fungible tokens - Receive token.sql
@@ -1,22 +1,25 @@
-
-SELECT b.block_timestamp,
-       r.predecessor_account_id from_account,
-       b.block_height,
-       convert_from(decode(ara.args ->> 'args_base64', 'base64'), 'UTF8') args_base64,
-       r.originated_from_transaction_hash transaction_hash,
-       ara.args ->'args_json'->>'amount' amount_transferred,
-       'token' currency_transferred, /* TODO: Show the actual token symbol (determined by the receiver at the top level instead of in the JSON args?)? */
-       ara.args->'args_json'->>'receiver_id' receiver_owner_account
-FROM receipts r
-    INNER JOIN execution_outcomes eo ON eo.receipt_id = r.receipt_id
-    INNER JOIN blocks b ON b.block_hash = r.included_in_block_hash
-    INNER JOIN action_receipt_actions ara ON ara.receipt_id = r.receipt_id
-WHERE 
+SELECT
+  b.block_timestamp,
+  r.predecessor_account_id from_account,
+  b.block_height,
+  convert_from(decode(ara.args ->> 'args_base64', 'base64'), 'UTF8') args_base64,
+  r.originated_from_transaction_hash transaction_hash,
+  ara.args -> 'args_json' ->> 'amount' amount_transferred,
+  'token' currency_transferred,
+  /* The ticker symbol later gets looked up in `backend/src/helpers/getCurrency.ts`. */
+  ara.args -> 'args_json' ->> 'receiver_id' receiver_owner_account
+FROM
+  receipts r
+  INNER JOIN execution_outcomes eo ON eo.receipt_id = r.receipt_id
+  INNER JOIN blocks b ON b.block_hash = r.included_in_block_hash
+  INNER JOIN action_receipt_actions ara ON ara.receipt_id = r.receipt_id
+WHERE
   eo.status IN ('SUCCESS_RECEIPT_ID', 'SUCCESS_VALUE')
   AND ara.action_kind = 'FUNCTION_CALL'
-  AND COALESCE(ara.args::json->>'method_name', '') = 'ft_transfer'
+  AND COALESCE(ara.args::json ->> 'method_name', '') = 'ft_transfer'
+  AND ara.args ->> 'args_json'::text IS NOT NULL
   AND ara.args -> 'args_json' ->> 'receiver_id' = $1
   AND b.block_timestamp > $2
-
-ORDER BY b.block_timestamp
+ORDER BY
+  b.block_timestamp
 LIMIT $3
